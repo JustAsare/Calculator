9c6793d0445150c80f1c74351e3f9dfda6bbdae5
Justice Asare <141356859+JustAsare@users.noreply.github.com>
2023-08-03 18:40:19 +0000

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,23 @@
+
+
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
+
+print("Choose Calculatation: \n",
+      "Add \n"
+      "Subtract \n"
+      "Multiply \n"
+      "Divide")
+
+calc = input()
+
+if calc == 'Add':
+    ans = num1 + num2
+elif calc == 'Subtract':
+    ans = num1 - num2
+elif calc == 'Multiply':
+    ans = num1 * num2
+elif calc == 'Divide':
+    ans = num1 / num2
+
+print(ans)
